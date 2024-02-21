11aa95805968cab084688216e3eef03203090fd8
liem nguyen <elminguyen@gmail.com>
2024-02-21 21:55:19 +0200

    likes ready

--- src/components/Likes.tsx
+++ src/components/Likes.tsx
@@ -1,11 +1,15 @@
-import { useReducer } from "react";
-import { Like } from "../types/DBtypes";
+import { useEffect, useReducer } from "react";
+import { Like, MediaItemWithOwner } from "../types/DBtypes";
+import { useLike } from "../hooks/apiHooks";
 
+//  count: number of likes
+// userLike: user's like, null if user hasn't liked
 type LikeState = {
   count: number;
   userLike: Like | null;
 };
 
+// setLikeCount: set total likes on that media???
 type LikeAction = {
   type: 'setLikeCount' | 'like';
   like?: Like | null;
@@ -17,6 +21,7 @@ const likeInitialState: LikeState = {
   userLike: null,
 };
 const LikeReducer = (state: LikeState, action: LikeAction): LikeState => {
+  console.log('like reducer: ', state, action);
   switch (action.type) {
     case 'setLikeCount':
       return {...state, count: action.count ?? 0};
@@ -30,20 +35,81 @@ const LikeReducer = (state: LikeState, action: LikeAction): LikeState => {
   }
 }
 
-const Likes = () => {
-  const [LikeState, likeDispatch] = useReducer(LikeReducer, likeInitialState);
+const Likes = ({item}: {item: MediaItemWithOwner | null}) => {
+  const [likeState, likeDispatch] = useReducer(LikeReducer, likeInitialState);
+  const {postLike, deleteLike, getCountByMediaId, getUserLike} = useLike();
+  // get user like
+  const getLikes = async () => {
+    const token = localStorage.getItem('token');
+    if (!item || !token) {
+      return;
+    }
+    try {
+      const userLike = await getUserLike(item.media_id, token);
+      console.log('get user like', userLike);
+      likeDispatch({type: 'like', like: userLike});
+    } catch (e) {
+      likeDispatch({type: 'like', like: null});
+      console.log('get user like error', (e as Error).message);
+    }
+  };
+
+  // get like count
+  const getLikeCount = async () => {
+    // get like count and dispatch it to the state
+    if (!item) {
+      return;
+    }
+    try {
+      const countResponse = await getCountByMediaId(item.media_id);
+      likeDispatch({type: 'setLikeCount', count: countResponse.count});
+    } catch (e) {
+      likeDispatch({type: 'setLikeCount', count: 0});
+      console.log('get user like error', (e as Error).message);
+    }
+
+
+  };
+
+  // dung useEffect de render so luot likes ban dau
+  useEffect(() => {
+    getLikes();
+    getLikeCount();
+  }, [item]);
+
+  const handleLike = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!item || !token) {
+        return;
+      }
+      // If user has liked the media, delete the like. Otherwise, post the like.
+      if (likeState.userLike) {
+        // delete the like and dispatch the new like count to the state.
+        await deleteLike(likeState.userLike.like_id, token);
+        likeDispatch({type: 'setLikeCount', count: likeState.count - 1});
+        likeDispatch({type: 'like', like: null});
+      } else {
+        // post the like and dispatch the new like count to the state. Dispatching is already done in the getLikes and getLikeCount functions.
+        await postLike(item.media_id, token);
+        getLikes();
+        getLikeCount();
+      }
+    } catch (e) {
+      console.log('like error', (e as Error).message);
+    }
+
+    console.log('like State', likeState);
+  };
+
   return (
     <>
-      Likes count: x
+      Likes count: {likeState.count}
       <button
         className=" bg-slate-700 p-2 hover:bg-slate-950"
-        onClick={() => {
-          console.log('item liked');
-          // post like
-          likeDispatch({type: 'like', like: 'like result from api'});
-        }}
+        onClick={handleLike}
       >
-        Like
+        {likeState.userLike ? 'Unlike' : 'Like'}
       </button>
     </>
   );
--- src/hooks/apiHooks.ts
+++ src/hooks/apiHooks.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../lib/functions";
-import { MediaItem, MediaItemWithOwner, User } from "../types/DBtypes";
+import { Like, MediaItem, MediaItemWithOwner, User } from "../types/DBtypes";
 import { Credentials } from "../types/Localtypes";
-import { LoginResponse, MediaResponse, UploadResponse, UserResponse } from "../types/MessageTypes";
+import { LoginResponse, MediaResponse, MessageResponse, UploadResponse, UserResponse } from "../types/MessageTypes";
 
 // const useMedia = (): MediaItemWithOwner[] => {  -> type comes automatically
 const useMedia = () => {
@@ -157,34 +157,60 @@ const useFile = () => {
 
 const useLike = () => {
   const postLike = async (media_id: number, token: string) => {
-    // TODO: Send a POST request to /likes with object { media_id } and the token in the Authorization header.
+    // Send a POST request to /likes with object { media_id }
+    // and the token in the Authorization header.
     const options: RequestInit = {
       method: 'POST',
       headers: {
         Authorization: 'Bearer ' + token,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(media_id),
+      body: JSON.stringify({media_id}),
     };
 
-    await fetchData<UserResponse>(
+    return await fetchData<MessageResponse>(
       import.meta.env.VITE_MEDIA_API + '/likes',
       options,
     );
-
-
   };
 
   const deleteLike = async (like_id: number, token: string) => {
-    // TODO: Send a DELETE request to /likes/:like_id with the token in the Authorization header.
+    // TODO: Send a DELETE request to /likes/:like_id with the
+    // token in the Authorization header.
+    const options: RequestInit = {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + token,
+
+      },
+    };
+
+    return await fetchData<MessageResponse>(
+      import.meta.env.VITE_MEDIA_API + '/likes/' + like_id,
+      options,
+    );
   };
 
   const getCountByMediaId = async (media_id: number) => {
     // TODO: Send a GET request to /likes/:media_id to get the number of likes.
+    return await fetchData<{count: number}>(
+      import.meta.env.VITE_MEDIA_API + '/likes/count/' + media_id,
+    );
   };
 
   const getUserLike = async (media_id: number, token: string) => {
-    // TODO: Send a GET request to /likes/bymedia/user/:media_id to get the user's like on the media.
+    // Send a GET request to /likes/bymedia/user/:media_id
+    // to get the user's like on the media.
+    const options: RequestInit = {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    return await fetchData<Like>(
+      import.meta.env.VITE_MEDIA_API + '/likes/bymedia/user/' + media_id,
+      options,
+    );
   };
 
   return {postLike, deleteLike, getCountByMediaId, getUserLike};
